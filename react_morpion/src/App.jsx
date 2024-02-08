14916d552868afd718ff3151ba13db36b08b56a1
Osirus Jack <methanblun@gmail.com>
2024-02-08 17:04:17 +0000

    button bug fixed

--- react_morpion/src/App.jsx
+++ react_morpion/src/App.jsx
@@ -1,35 +1,46 @@
 
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
 
- function Square({value}) {
+ function Square() {
+  const [value, setValue] = useState(null);
 
-      return <button className='square'>{value}</button>
-       }
+  function handleClick() {
+    setValue('X');
+  }
 
   return (
-    <>
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 
+ }   
 
-   
-<div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+  return (
+    <div className='fragment'>
+  <div className="board-row">
+        <Square  />
+        <Square  />
+        <Square  />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square  />
+        <Square  />
+        <Square  />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square  />
+        <Square  />
+        <Square  />
       </div>
-    </>
+    </div>
   )
 }
 
